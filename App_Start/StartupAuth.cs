1f8a3eb1bb0d53eb3f497aa28e6f24d11b4faaf8
Tyler Doerksen <tylerd@microsoft.com>
2020-07-28 10:21:26 -0500

    initial commit

---
+++ App_Start/StartupAuth.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace AADWebFormsApplication1
+{
+    public partial class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        private static string aadInstance = EnsureTrailingSlash(ConfigurationManager.AppSettings["ida:AADInstance"]);
+        private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        private string authority = aadInstance + tenantId;
+        // This is the resource ID of the AAD Graph API.  We'll need this to request a token to call the Graph API.
+        private static string graphResourceId = "https://graph.windows.net";
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+
+            // By default, the claims mapping will map claim names in the old format to accommodate older SAML applications.
+            // 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role' instead of 'roles'
+            // This flag ensures that the ClaimsIdentity claims collection will be built from the claims in the token
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        NameClaimType = "name",
+                        RoleClaimType = "groups",    // The claim in the Jwt token where App roles are provided.
+                    }
+                }
+                );
+
+            // This makes any middleware defined above this line run before the Authorization rule is applied in web.config
+            app.UseStageMarker(PipelineStage.Authenticate);
+        }
+
+        private static string EnsureTrailingSlash(string value)
+        {
+            if (value == null)
+            {
+                value = string.Empty;
+            }
+
+            if (!value.EndsWith("/", StringComparison.Ordinal))
+            {
+                return value + "/";
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AADWebFormsApplication1.Global" Language="C#" %>
